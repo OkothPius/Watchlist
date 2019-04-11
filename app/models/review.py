dea4268287bb3262cb1c4bc9c5b73e2a8c6e15bc
OkothPius <orukopius8@gmail.com>
2019-04-11 11:25:51 +0300

    Save everything

--- app/models/review.py
+++ app/models/review.py
@@ -8,11 +8,17 @@ class Review:
         self.imageurl = imageurl
         self.review = review
 
+
     def save_review(self):
         Review.all_reviews.append(self)
 
+
     @classmethod
-    def clear_reviews(cls,id):
+    def clear_reviews(cls):
+        Review.all_reviews.clear()
+
+    @classmethod
+    def get_reviews(cls,id):
 
         response = []
 
@@ -20,4 +26,4 @@ class Review:
             if review.movie_id == id:
                 response.append(review)
 
-        return response        
+        return response
---
+++ config.py
@@ -0,0 +1,2 @@
+MOVIE_API_KEY = 'b595f1c3a2ee9e4a80e9974540709b53'
+SECRET_KEY = 'orukopius'
